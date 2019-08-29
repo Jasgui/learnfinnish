0bb2a4238d587f4c7d348fc5d6c81c8ca4c5e71b
Guillaume <g.jaskula@me.com>
2019-08-29 20:58:37 +0300

    Add files via upload

---
+++ learnfinnish-api/server.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+
+app.listen(3000, function () {
+            console.log("api running);
+            });
