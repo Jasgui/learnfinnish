83d005a2e8f3e94333a276f02cdc947caf8b7135
Guillaume <g.jaskula@me.com>
2019-08-30 23:41:54 +0300

    Add files via upload

--- learnfinnish-api/server.js
+++ learnfinnish-api/server.js
@@ -1,7 +1,44 @@
-var express = require('express');
-var app = express();
-var mongoose = require('mongoose');
+//var express = require('express');
+//var app = express();
 
-app.listen(3000, function () {
-    console.log("api running");
+
+//app.listen(3000, function () {
+//    console.log("api running");
+//});
+
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://159.65.55.177/learnfinnish', {
+    useNewUrlParser: true
+});
+
+var kittySchema = new mongoose.Schema({
+    name: String
+});
+kittySchema.methods.speak = function () {
+    var greetings = this.name ?
+        "Meow name is " + this.name :
+        "I don't have a name";
+
+}
+var Kitten = mongoose.model('Kitten', kittySchema);
+
+var silence = new Kitten({
+    name: 'Silence'
+});
+var fluffy = new Kitten({
+    name: 'Fluffy'
+});
+
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    fluffy.save(function (err, fluffy) {
+        if (err) return console.error(err);
+        fluffy.speak();
+
+    });
+
+
 });
