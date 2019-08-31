c18e0da2b136f577270396743d178dde54331dc9
Guillaume <g.jaskula@me.com>
2019-08-31 14:46:11 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ app.get('/', function (req, res) {
 
 });
 
+app.get('/funions', function (req, res) {
+
+    res.send("funions are here");
+
+});
+
 app.listen(3000, function () {
     console.log("API is running on port 3000");
 });
