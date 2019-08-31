b155faed408f0103d55c206e587d21b86d9e9ceb
Guillaume <g.jaskula@me.com>
2019-08-31 14:32:04 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,43 +1,12 @@
-//var express = require('express');
-//var app = express();
+var express = require('express');
+var app = express();
 
+app.get('/', function (req, res) {
 
-//app.listen(3000, function () {
-//    console.log("api running");
-//});
+    res.send("my first api");
 
-
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/learnfinnish', {
-    useNewUrlParser: true
 });
 
-var kittySchema = new mongoose.Schema({
-    name: String
-});
-kittySchema.methods.speak = function () {
-    var greetings = this.name ?
-        "Meow name is " + this.name :
-        "I don't have a name";
-
-}
-var Kitten = mongoose.model('Kitten', kittySchema);
-
-var silence = new Kitten({
-    name: 'Silence'
-});
-var fluffy = new Kitten({
-    name: 'Fluffy'
-});
-
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
-    silence.save(function (err, silence) {
-        if (err) return console.error(err);
-
-    });
-
-
+app.listen(3000, function () {
+    console.log("API is running on port 3000");
 });
