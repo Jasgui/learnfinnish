91f686a1a0b7b14e343a8aaea67072bfb7675ce2
Guillaume <g.jaskula@me.com>
2019-08-31 19:20:48 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var db = mongoose.connect('mongodb:127.0.0.1:27017/testdb');
+mongoose.connect('mongodb://127.0.0.1:27017', {
+    useNewUrlParser: true
+});
+var db = mongoose.connection;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
