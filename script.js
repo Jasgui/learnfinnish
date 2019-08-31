531cd764855b6ad81145b4f3e747c83f223b521f
Guillaume <g.jaskula@me.com>
2019-09-01 01:24:34 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const button = document.getElementById('button');
-import HttpService from './services/http-service';
+import HttpService from '/services/http-service';
 
 const http = new HttpService();
 
