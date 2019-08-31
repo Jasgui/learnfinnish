369ab26768037a475893ec6b888c06ec5c374da5
Guillaume <g.jaskula@me.com>
2019-08-31 19:04:54 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,69 +1,14 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var db = mongoose.connect('mongodb:127.0.0.1:27017/testdb');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 
-
-var ingredients = [
-    {
-        "id": "123H",
-        "text": "eggs"
-  },
-    {
-        "id": "943h",
-        "text": "milk"
-  },
-    {
-        "id": "3405J",
-        "text": "bacon"
-  },
-    {
-        "id": "9537h",
-        "text": "flour"
-  },
-];
-
-app.get('/ingredients', function (req, res) {
-
-    res.send(ingredients);
-
-});
-
-app.post('/ingredients', function (req, res) {
-    var ingredient = req.body;
-    if (!ingredient || ingredient.text === "") {
-        res.status(500).send({
-            error: "your ingredient must have text"
-        });
-    } else {
-        ingredients.push(ingredient);
-        res.status(200).send(ingredients);
-    }
-});
-
-
-app.put('/ingredients/:ingredientId', function (req, res) {
-
-    var newText = req.body.text;
-    if (!newText || newText === "") {
-        res.status(500).send({
-            error: "you must provide ingredient text"
-        });
-    } else {
-        for (var x = 0; x < ingredients.length; x++) {
-            var ing = ingredients[x];
-            if (ing.id === req.params.ingredientId) {
-                ingredients[x].text = newText;
-                break;
-            }
-        }
-        res.send(ingredients);
-    }
-});
 
 
 app.listen(3000, function () {
