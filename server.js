aacdef2c73ad41ab9417830d3cda4288a7696f0e
Guillaume <g.jaskula@me.com>
2019-08-31 21:05:24 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ mongoose.connect('mongodb://127.0.0.1:27017/testdatabase', {
 });
 var db = mongoose.connection;
 
+var Product = require('./model/product');
+var Wishlist = require('./model/wishlist');
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
