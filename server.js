bbb862d760cfcd4dfc8287fc34dc30187189789e
Guillaume <g.jaskula@me.com>
2019-09-01 00:38:37 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -16,6 +16,13 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 
+app.all('/*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET");
+    next();
+});
+
 app.post('/product', function (req, res) {
     var product = new Product();
     product.title = req.body.title;
