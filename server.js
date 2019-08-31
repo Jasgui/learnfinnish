1e9dd15bcac2454efbc3ffbd16b5afe902ac97bc
Guillaume <g.jaskula@me.com>
2019-08-31 15:28:28 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,17 +1,34 @@
 var express = require('express');
 var app = express();
+var bodyparser = require('body-parser');
+
+
+var ingredients = [
+    {
+        "id": "123H",
+        "text": "eggs"
+  },
+    {
+        "id": "943h",
+        "text": "milk"
+  },
+    {
+        "id": "3405J",
+        "text": "bacon"
+  },
+    {
+        "id": "9537h",
+        "text": "flour"
+  },
+];
 
 app.get('/', function (req, res) {
 
-    res.send("my first api");
+    res.send(ingredients);
 
 });
 
-app.get('/funions', function (req, res) {
 
-    res.send("funions are here");
-
-});
 
 app.listen(3000, function () {
     console.log("API is running on port 3000");
