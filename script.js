9149736e3d2904224e7d9cf82376a551f1fc6fd5
Guillaume <g.jaskula@me.com>
2019-09-01 14:46:39 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 const button = document.getElementById('button');
 const textcheck = document.getElementById('textcheck');
-//import HttpService from './services/http-service.js';
 
-//const http = new HttpService();
+
+
+
+
 
 
 
 button.onclick = function () {
 
     console.log("at least the button works");
-    //http.getproducts();
     fetch('http://learnfinnish.xyz:3000/product').then(function (response) {
         return response.json();
     }).then(function (data) {
