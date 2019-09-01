f64ac350ff4fed48e92311e383ddf7c233ca306a
Guillaume <g.jaskula@me.com>
2019-09-01 12:51:39 +0300

    Add files via upload

--- services/http-service.js
+++ services/http-service.js
@@ -1,11 +1,19 @@
 class HttpService {
     getProducts = () => {
-
-        fetch('http://learnfinnish.xyz:3000/product')
-            .then(res => {
-                console.log(res.json());
-            })
+        console.log("this works");
     }
 }
+
+
+//
+//class HttpService {
+//    getProducts = () => {
+//
+//        fetch('http://learnfinnish.xyz:3000/product')
+//            .then(res => {
+//                console.log(res.json());
+//            })
+//    }
+//}
 
 export default HttpService;
