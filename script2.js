13b0889811cad4b4aef34677d1f95642e19b8a08
Guillaume <g.jaskula@me.com>
2019-09-21 21:46:46 +0300

    Add files via upload

--- script2.js
+++ script2.js
@@ -1,5 +1,26 @@
 const button = document.getElementById('button');
 const textcheck = document.getElementById('textcheck');
+const buttonPost = document.getElementById('buttonPost');
+
+buttonPost.onclick = () => {
+
+    const url = "http://learnfinnish.xyz:3000/product";
+
+    let product = {
+        title: "I don't know",
+        price: 39.99
+    };
+
+    let fetchData = {
+        method: 'POST',
+        body: product,
+        headers: new Headers()
+    };
+
+    fetch(url, fetchData)
+        .then((res) => console.log(res))
+
+};
 
 
 button.onclick = function () {
