66d5767a706b7581cfd81815ea1fe7722855b3fd
Jasgui <g.jaskula@me.com>
2019-09-28 21:37:59 +0300

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({
 app.all('/*', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "POST, GET");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT");
     next();
 });
 
