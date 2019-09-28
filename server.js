d5e609419dac34d57f6b6532e870489c017bab48
Jasgui <g.jaskula@me.com>
2019-09-28 16:28:20 +0300

    Update server.js

--- server.js
+++ server.js
@@ -313,34 +313,35 @@ app.get('/mistake', function (req, res) {
         }
     });
 });
-
-app.get('/list', function (req, res) {
-    List.find({}, function (err, items) {
-        if (err) {
-            res.status(500).send({
-                error: "Could not fetch items from review"
-            });
-        } else {
-            res.send(items);
-        }
-    });
-});
-
-
+//
 //app.get('/list', function (req, res) {
-//    List.find({}).populate({
-//        path: 'items',
-//        model: 'Allitems'
-//    }).exec(function (err, list) {
+//    List.find({}, function (err, items) {
 //        if (err) {
 //            res.status(500).send({
-//                error: "Could not fetch the list"
+//                error: "Could not fetch items from review"
 //            });
 //        } else {
-//            res.send(list);
+//            res.send(items);
 //        }
-//    })
+//    });
 //});
+
+
+
+app.get('/list', function (req, res) {
+    List.find({}).populate({
+        path: 'items',
+        model: 'Allitem'
+    }).exec(function (err, list) {
+        if (err) {
+            res.status(500).send({
+                error: "Could not fetch the list"
+            });
+        } else {
+            res.send(list);
+        }
+    })
+});
 
 
 
