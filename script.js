a94c88d096d340e69da8849fbce9a4bd41d191cc
Jasgui <g.jaskula@me.com>
2019-09-29 19:52:13 +0300

    Testing logic implemented on frontend

--- script.js
+++ script.js
@@ -5,9 +5,15 @@ const enterButton = document.getElementById('enterButton');
 const startbtn = document.getElementById('startbtn');
 
 var lessonContent = [];
+var counter = 0;
+var EOSRemoveFromLearn = [];
+var EOSRemoveFromReview = [];
+var EOSRemoveFromMistake = [];
+var EOSAddToReview = [];
+var EOSAddToMistake = [];
+
 
 import {
-    test,
     normalise
 } from '/module.js';
 
@@ -19,37 +25,88 @@ startbtn.onclick = () => {
         .then((res) => res.json())
         .then(function (data) {
             lessonContent = [...data];
-
-            for (let i = 0; i < lessonContent.length; i++) {
-                test(lessonContent[i]);
-            }
+            counter = 0;
+            test(lessonContent[counter]);
 
         })
 
 }
 
 
+function test = (content) => {
+
+    question.innerHTML = content.french;
+    answer.value = "";
+    correction.innerHTML = "";
+
+};
 
 
-
-
+function endOfSession = () => {
+    console.log("End of session");
+    console.log(EOSRemoveFromLearn);
+    console.log(EOSRemoveFromReview);
+    console.log(EOSRemoveFromMistake);
+    console.log(EOSAddToReview);
+    console.log(EOSAddToMistake);
+}
 
 enterButton.onclick = () => {
-    if (normalise(answer.value) === normalise(phrases[counter].value)) {
+    if (normalise(answer.value) === normalise(lessonContent[counter].finnish)) {
         correction.innerHTML = "yes!";
-        reviews.push(phrases[counter]);
+
+        if (lessonContent[counter].score === 0) {
+            lessonContent[counter].score = 4;
+            EOSRemoveFromLearn.push(lessonContent[counter]);
+            EOSAddToReview.push(lessonContent[counter]);
+        } else if (lessonContent[counter].score === 4) {
+            EOSRemoveFromReview.push(lessonContent[counter]);
+            EOSAddToReview.push(lessonContent[counter]);
+        } else if (lessonContent[counter].score === 1) {
+            lessonContent[counter].score = 4;
+            EOSRemoveFromMistake.push(lessonContent[counter]);
+            EOSAddToReview.push(lessonContent[counter]);
+        } else {
+            lessonContent[counter].score--;
+            EOSRemoveFromMistake.push(lessonContent[counter]);
+            EOSAddToMistake.push(lessonContent[counter]);
+        }
+
         counter++;
-        test(phrases[counter]);
+        if (counter > lessonContent.length) {
+            endOfSession();
+        } else {
+            test(lessonContent[counter]);
+        }
+
     } else {
-        correction.innerHTML = phrases[counter].value;
-        mistakes.push(phrases[counter]);
+
+        correction.innerHTML = lessonContent[counter].finnish;
+
+        if (lessonContent[counter].score === 0) {
+            lessonContent[counter].score = 3;
+            EOSRemoveFromLearn.push(lessonContent[counter]);
+            EOSAddToMistake.push(lessonContent[counter]);
+        } else if (lessonContent[counter].score === 4) {
+            lessonContent[counter].score = 3;
+            EOSRemoveFromReview.push(lessonContent[counter]);
+            EOSAddToMistake.push(lessonContent[counter]);
+        } else {
+            EOSRemoveFromMistake.push(lessonContent[counter]);
+            EOSAddToMistake.push(lessonContent[counter]);
+        }
+
         counter++;
-        setTimeout(function () {
-            test(phrases[counter])
-        }, 5000);
+        if (counter > lessonContent.length) {
+            endOfSession();
+        } else {
+            setTimeout(function () {
+                test(lessonContent[counter])
+            }, 5000);
+        }
+
     }
-    console.log(mistakes);
-    console.log(reviews);
+
 }
 
 
