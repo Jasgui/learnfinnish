f45da024e7affcef918e59f89bca0e47408c39ef
Jasgui <g.jaskula@me.com>
2019-10-13 22:15:52 +0300

    Update testpython.py

--- testpython.py
+++ testpython.py
@@ -1,7 +1,37 @@
 from flask import Flask
+from flask import request
+import sys
+import optparse
+import time
+import hfst
+import json
 
-app = flask(__name__)
+app = Flask(__name__)
 
-@app.route('/test')
-def helloworld():
-    return 'hello, world!'
+start = int(round(time.time()))
+
+@app.route("/lookup", methods=['POST'])
+def hello_world():
+    
+    req_data = request.get_json()
+    item = req_data['item']
+    
+    
+    
+    filename = "./finntreebank.hfst"
+    input_stream = hfst.HfstInputStream(filename)
+    analyser = input_stream.read()
+    result = analyser.lookup(item)
+    print (result[0][0])
+    resultObj = result[0][0]
+    print(resultObj)
+    return(resultObj)
+
+if __name__ == '__main__':
+    parser = optparse.OptionParser(usage="python simpleapp.py -p <port>")
+    parser.add_option('-p', '--port', action='store', dest='port', help='The port to listen on.')
+    (args, _) = parser.parse_args()
+    if args.port == None:
+        print ("Missing required argument: -p/--port")
+        sys.exit(1)
+    app.run(host='0.0.0.0', port=int(args.port), debug=False)
